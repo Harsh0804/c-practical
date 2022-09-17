20fb906b9a65858c087bf6d6d16005d50e7a991a
pc1 <singhharshvisen16@gmail.com>
2022-09-17 12:53:43 +0530

    hlo

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-int=-1
-int a[5];
+int top=-1
+int a[7];
 void push();
 void pop();
 void display();
@@ -13,6 +13,35 @@ int main()
     {
         printf("\n Enter 1.insert\n 2.delete\n 3.display")
         scanf("%d",&id);
+        if(id==1)
+        {
+            push();
+        }
+        else if(id==2)
+        {
+            pop();
+        }
+        else if(id==3)
+        {
+            display();
+        }
+        else
+        {
+            exit(0);
+        }
 
+
+
+    }
+}
+
+
+void push()
+{
+    int i;
+    top++;
+    if(top>=5-1)
+    {
+        
     }
 }
