d06fd8eaa7999a3b23606853c14c74e133678e7c
pc1 <singhharshvisen16@gmail.com>
2022-09-17 12:48:18 +0530

    hlo

--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+int=-1
+int a[5];
+void push();
+void pop();
+void display();
+
+int main()
+{
+    int id;
+    while(1)
+    {
+        printf("\n Enter 1.insert\n 2.delete\n 3.display")
+        scanf("%d",&id);
+
+    }
+}
